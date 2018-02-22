41291c1fd3bf1b291ad8bf3a0b7a857bb2534a87
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-02-22 19:59:19 +0000

    Update app.js

--- app.js
+++ app.js
@@ -24,37 +24,8 @@ client.on('ready', () => {
   case "help":
   message.author.sendMessage("Hello there! ");
   break;
-  
-  case "support":
-let help = message.author
-message.reply(" we are contacting staff. Please wait a moment.")
-let staffc =  message.guild.channels.find("name", "support-staff")
-staffc.sendMessage(`<@&416285748942077963>, ${message.author.username} needs help in <#416284519453163521>!`)
-break ;
            
-  case "info":
-           message.reply("check your DMs!");
-  var embedinfo = new Discord.RichEmbed()
-.setFooter("© BloxiCord | Owned by The Bot Company & predator.co!")
-.setColor("0x7DCEA0")
-.setTitle("📖 BloxiCord Information")
-.setDescription("For other inquiries that this information doesn't cover, use `~help`.")
-.addField(":iphone: What is BloxiCord?", "BloxiCord is a discord bot that is used to replace lots of bots. There are many features, from linking ROBLOX accounts to discord through to administration. If you want to see a full list of our commands please run `~cmds`!")
-.addField(":pencil2: How do I use BloxiCord?", "BloxiCord is designed to be a simple, fast and easy solution to all the bots that are found in a regular discord server. More than 90% of the people we asked had over 3 bots that did different things in their server, we are coming in to try and lower that in an even more simpler fasion. If our `~cmds` and `~help` commands didn't help, join our official discord server down below. We have an active support team waiting to help you!")
-.addField(":telephone_receiver: Support?", "Struggling with BloxiCord? Found a bug? Have a suggestion of a feature you want us to add? Feel free to join our discord offical server (link below)! If you do require help simply head to the support channel and run the `~support` command!")
-.addField(":moneybag: How much does it cost?", "The regular bot is free! When we release the paid version, it will have more functions but BloxiCord will always be free.")
-.addField(":inbox_tray: BloxiCord Bot Invite", "**Click this button for an invite link:** [<:radio_button:382568980218511361>](https://discordapp.com/oauth2/authorize?client_id=414823685770444801&scope=bot&permissions=502627454)")
-.addField(":pushpin: Official Server", "https://discord.gg/WxUPp3P")
-.addField(":closed_book: Our Website", "**COMING SOON**")
-message.author.sendEmbed(embedinfo);
-  break;
-           
-  defult:
-  message.channel.sendMessage(" :no_entry:  That command is invalied. The prefix is **~**. For a list of valid commands do **~cmds!**");
-    } 
-})
-
-       case "purge":
+                  case "purge":
            // We have to wrap this in an async since awaits only work in them.
         async function purge() {
             message.delete(); // Let's delete the command message, so it doesn't interfere with the messages we are going to delete.
@@ -87,5 +58,34 @@ message.author.sendEmbed(embedinfo);
 
     }
 });
+  
+  case "support":
+let help = message.author
+message.reply(" we are contacting staff. Please wait a moment.")
+let staffc =  message.guild.channels.find("name", "support-staff")
+staffc.sendMessage(`<@&416285748942077963>, ${message.author.username} needs help in <#416284519453163521>!`)
+break ;
+           
+  case "info":
+           message.reply("check your DMs!");
+  var embedinfo = new Discord.RichEmbed()
+.setFooter("© BloxiCord | Owned by The Bot Company & predator.co!")
+.setColor("0x7DCEA0")
+.setTitle("📖 BloxiCord Information")
+.setDescription("For other inquiries that this information doesn't cover, use `~help`.")
+.addField(":iphone: What is BloxiCord?", "BloxiCord is a discord bot that is used to replace lots of bots. There are many features, from linking ROBLOX accounts to discord through to administration. If you want to see a full list of our commands please run `~cmds`!")
+.addField(":pencil2: How do I use BloxiCord?", "BloxiCord is designed to be a simple, fast and easy solution to all the bots that are found in a regular discord server. More than 90% of the people we asked had over 3 bots that did different things in their server, we are coming in to try and lower that in an even more simpler fasion. If our `~cmds` and `~help` commands didn't help, join our official discord server down below. We have an active support team waiting to help you!")
+.addField(":telephone_receiver: Support?", "Struggling with BloxiCord? Found a bug? Have a suggestion of a feature you want us to add? Feel free to join our discord offical server (link below)! If you do require help simply head to the support channel and run the `~support` command!")
+.addField(":moneybag: How much does it cost?", "The regular bot is free! When we release the paid version, it will have more functions but BloxiCord will always be free.")
+.addField(":inbox_tray: BloxiCord Bot Invite", "**Click this button for an invite link:** [<:radio_button:382568980218511361>](https://discordapp.com/oauth2/authorize?client_id=414823685770444801&scope=bot&permissions=502627454)")
+.addField(":pushpin: Official Server", "https://discord.gg/WxUPp3P")
+.addField(":closed_book: Our Website", "**COMING SOON**")
+message.author.sendEmbed(embedinfo);
+  break;
+           
+  defult:
+  message.channel.sendMessage(" :no_entry:  That command is invalied. The prefix is **~**. For a list of valid commands do **~cmds!**");
+    } 
+})
 
 client.login(process.env.TOKEN);
