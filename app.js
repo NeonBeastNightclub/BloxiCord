75ea720ba9190d574a6641460d05c64f2529cb2d
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-02-24 09:59:11 +0000

    Update app.js

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ let help = message.author
 message.reply(" we are contacting staff. Please wait a moment.")
 let staffc =  message.guild.channels.find("name", "support-staff")
 staffc.sendMessage(`<@&416285748942077963>, ${message.author.username} needs help in <#416284519453163521>!`)
-break ;    
+break;    
  } else {
 message.reply("Unfortunately this can only be done in our customer support channel. To join our server run `~join` or `~info`!")
-     break;
+break;
 
 });
 
