c395f611bdf4e5e6d1e90e9d9bf08ba7494a12d0
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-01 16:49:43 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,101 +1,56 @@
 // BloxiCord Discord Bot Code
-// Version: 0.5 PRE-ALPHA PRE-REALEASE
+// Version: 0.9 PRE-ALPHA / PRE-REALEASE
 // Discord Code: WxUPp3P
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const db = require('quick.db');
-const prefix = '~';
+const weather = require('weather-js'); // Will be coming at a later date on release.
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs on BloxiCord.`);
-    client.user.setActivity('over ' + client.guilds.size + ' servers! | ~help', { type: 'WATCHING' });
+    client.user.setActivity('on ' + client.guilds.size + ' servers! | ~help', { type: 'WATCHING' });
     client.user.setStatus("online");
-  });
+});
 
-  client.on("message", function(message) {    
 
- if (message.channel.type != 'text') return message.channel.send("All commands must be used in a server with BloxiCord. Not in DM's");
+client.on('message', message => {
 
-db.fetchObject(`guildPrefix_${message.guild.id}`).then(i => {
+    let sender = message.author;
 
-let prefix;
-    
-if (i.text) {
-    prefix = i.text
-} else {
-    prefix = '~' // DEFULT
-}
+    if (sender.bot) return;
+    if (message.channel.type === 'dm') {
+        message.channel.send("**Unfortunately we can only read things in guilds (servers). Please use our commands in a server with `BloxiCord` in!** *If you believe there is a problem, please contact a server administator or contact our support team.*")
+        return;
+    }
 
-  if (message.author.equals(client.user)) return;
+    var guildid = message.guild.id
+    db.fetch(`guildPrefix_${guildid}`).then(i => {
 
-  if (!message.content.startsWith(prefix)) return;
+        let prefix = i || '~'
 
-  var args = message.content.substring(prefix.length).split(" ");
-  https://github.com/iTrxpical/BloxiCord/pulls
-   switch (args[0].toLowerCase()) {
-           
-  case "help":
-  message.author.sendMessage("Hello there! ");
-  break;
-           
-  case "setprefix":
-if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("You have not got the permissions, if there is a problem contact the server administrator to run the command `~setup`!");
-if (args.join(" ")) return message.channel.send("**Incorrect Usage**. `~setprefix <prefix>`");
+        let msg = message.content.toLowerCase();
+        let args = message.content.slice(prefix.length).trim().split(" ");
+        let cmd = args.shift().toLowerCase();
 
-db.updateText(`guildPrefix_${message.guild.id}`, args.join().trim()).then(i => {
+        if (!message.content.startsWith(prefix)) return;
 
-message.channel.send('__**Prefix Changed To:**__' + i.text);
+        try {
 
+            let commandFile = require(`./commands/${cmd}.js`);
+            commandFile.run(Discord, client, message, args);
+
+        } catch (e) {
+
+            console.log(e);
+
+        } finally {
+
+            console.log(`${message.author.username} ran the command: ${cmd} sucessfully!`);
+
+        }
+
+    })
 })
-
-case "purge":
-
-if (isNaN(args[1])) return message.channel.send("**Please Supply a Valid Number!**");
-if (args[1] > 100) return message.channel.send("**In a single purge, there is a max of 100 messages.**");
-
-message.channel.bulkDelete(args[1])
-.then ( messages => message.channel.send(`**Successfully Deleted \`${messages.size}/${args[1]}\` messages.**`)
-.then(message => {
-    message.delete(10000)
-  }))
-break;
-           
-case "support":
-if (message.channel.id === "416284519453163521") {
-let help = message.author
-message.reply(" we are contacting staff. Please wait a moment.")
-let staffc =  message.guild.channels.find("name", "support-staff")
-staffc.sendMessage(`<@&416285748942077963>, ${message.author.username} needs help in <#416284519453163521>!`)
-break;    
- } else {
-message.reply("Unfortunately this can only be done in our customer support channel. To join our server run `~join` or `~info`!")
-return;
-};
- 
-           
-  case "info":
-           message.reply("check your DMs!");
-  var embedinfo = new Discord.RichEmbed()
-.setFooter("© BloxiCord | Owned by The Bot Company & predator.co!")
-.setColor("0x7DCEA0")
-.setTitle("📖 BloxiCord Information")
-.setDescription("For other inquiries that this information doesn't cover, use `~help`.")
-.addField(":iphone: What is BloxiCord?", "BloxiCord is a discord bot that is used to replace lots of bots. There are many features, from linking ROBLOX accounts to discord through to administration. If you want to see a full list of our commands please run `~cmds`!")
-.addField(":pencil2: How do I use BloxiCord?", "BloxiCord is designed to be a simple, fast and easy solution to all the bots that are found in a regular discord server. More than 90% of the people we asked had over 3 bots that did different things in their server, we are coming in to try and lower that in an even more simpler fasion. If our `~cmds` and `~help` commands didn't help, join our official discord server down below. We have an active support team waiting to help you!")
-.addField(":telephone_receiver: Support?", "Struggling with BloxiCord? Found a bug? Have a suggestion of a feature you want us to add? Feel free to join our discord offical server (link below)! If you do require help simply head to the support channel and run the `~support` command!")
-.addField(":moneybag: How much does it cost?", "The regular bot is free! When we release the paid version, it will have more functions but BloxiCord will always be free.")
-.addField(":inbox_tray: BloxiCord Bot Invite", "**Click this button for an invite link:** [<:radio_button:382568980218511361>](https://discordapp.com/oauth2/authorize?client_id=414823685770444801&scope=bot&permissions=502627454)")
-.addField(":pushpin: Official Server", "https://discord.gg/WxUPp3P")
-.addField(":closed_book: Our Website", "**COMING SOON**")
-message.author.sendEmbed(embedinfo);
-  break;
-           
-  defult:
-  message.channel.sendMessage(" :no_entry:  That command is invalied. The prefix is **~**. For a list of valid commands do **~cmds!**");
-    } 
-})
-
-  })
 
 client.login(process.env.TOKEN);
