19cc4d64317e41c0d283f7aa5899aa29e862c7a4
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-02 14:15:10 +0000

    Update guildmessage.js

--- commands/guildmessage.js
+++ commands/guildmessage.js
@@ -2,9 +2,9 @@ exports.run = (Discord, client, message, args) => {
 
 if (!message.author.id === '255048840615428107') return;
 
-    var guildList = client.channels.first();
+    var guildList = client.guilds.array();
 try {
-    guildList.forEach(guild => guild.defaultChannel.send(args));
+    guildList.forEach(guild => guild.channel.first().send(args));
 } catch (err) {
     console.log("Could not send message to " + guild.name);
 }
